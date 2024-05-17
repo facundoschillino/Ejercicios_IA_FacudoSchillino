2756e4f47d78d4ae709fc81ce69e4d7b2ea07dd0
Facundo Gabriel Schillino Gariglio <facundoschillino01@gmail.com>
2024-05-17 17:17:30 -0300

    .,

---
+++ Ejercicio_Ratabots.py
@@ -0,0 +1,74 @@
+from simpleai.search import (
+    SearchProblem,
+    breadth_first,
+    uniform_cost,
+    depth_first,
+    limited_depth_first,
+    iterative_limited_depth_first,
+    greedy,
+    astar,
+)
+from simpleai.search.viewers import BaseViewer, WebViewer
+#5x5
+GOAL = ((3,5),3, ()) ##Posicion a la que tiene que llegar, cantidad de comidas que tiene que adquirir, posicion de las comidas
+Initial_state = ((3,5), 0, ((1,2),(3,4),(4,0))) ##Me conviene mas pensarlo como un diccionario donde tengo : Posicion, cantidades comidas, y comidas restantes.
+Obstaculos = ((0,3),(0,5),(1,3),(1,1),(2,2),(2,4),(3,0),(4,1),(4,3),(4,5),(5,3))
+
+
+class Ratabots(SearchProblem):
+    def actions (self, state):
+        actions = [] #creo la lista acciones
+        posicion, comidas, comidas_restantes = state ##descuajeringamos
+        fila, columna = posicion ##volvemos a descuajeringar, pero ahora las posiciones
+        posibilidades = [] ## creamos una lista para anadir las posibles posiciones a las que se puede mover la ratta
+        posibilidades.append((fila +1, columna)) #abajo
+        posibilidades.append((fila -1, columna))#arriba
+        posibilidades.append((fila, columna+1))#derecha
+        posibilidades.append((fila, columna-1)) #izquierda
+        for casilla in posibilidades:
+            if (casilla[0] >= 0 and casilla[0] <= 5) and (casilla[1] >= 0 and casilla[1] <= 5): #Si esta dentro de los limites de la grilla.
+                Hay_un_obstaculo = (casilla in Obstaculos) #si no es un obstaculo
+                if Hay_un_obstaculo == False :
+                    actions.append((posicion,casilla))#origen, destino
+        return actions ##Consultar a fisa si debo considerar en las acciones comer, o si lo hago en el resultado cuando la rata esta encima de la casilla que posee la comida
+    
+    def results(self, state, actions):
+        origen, destino = actions
+        Comidas = state[2]
+        estado = list(state)
+        for pos,comida in enumerate(Comidas): ##Si hay una comida, la come
+            es_comida = (comida == destino)
+            if es_comida:
+                state[2].remove(comida)
+                estado[1] +=1 
+        estado [0] = destino
+        estado = tuple(estado)
+        return estado
+    
+    def is_goal(self, state):
+        return state == GOAL
+    
+    def cost(self, state, action, state2):
+        return 1
+    
+    def heuristics(self, state):
+        Comidas = state[2]
+        comidas_por_encontrar = Comidas.count()
+        return comidas_por_encontrar
+        
+
+my_problem = Ratabots(Initial_state)
+
+#v = BaseViewer()
+v = WebViewer()
+result = astar(my_problem, 1000, viewer= v)
+
+if result is None:
+    print("No solution")
+else:
+    for action, state in result.path():
+        print("A:", action)
+        print("S:")
+        print(*state, sep="\n")
+
+    print("Final cost:", result.cost)        
--- Pruebas.py
+++ Pruebas.py
@@ -1,29 +1,21 @@
-def actions(state):
-        actions = []
-        for JarroOrigen, JARcontenido in enumerate(state): #busco el jarro de origen
-            if JARcontenido > 1 :    #Si es mayor a uno puede trasvasar
-                for JarroDestino, JARROcontenido in enumerate(state): #Busco el jarro de destino
-                    if JarroDestino != JarroOrigen: #Verifico que no sea el mismo jarro
-                        if JARROcontenido != 1:   #Verifico que el jarro este vacío o que tenga más de uno(No se si darla esta lógica esta bien, creo que puede reventar)
-                            if JARROcontenido < JarroDestino + 1: #Verifico que el jarro de destino no este lleno para poder trasvasar
-                                actions.append((JarroOrigen,JarroDestino)) #Le doy los jarros a los que tiene que trasvasar
-        return actions
-
-def result (state, actions):
+GOAL = ((3,5),3, []) ##Posicion a la que tiene que llegar, cantidad de comidas que tiene que adquirir, posicion de las comidas
+Initial_state = ((3,5), 0, [(1,2),(3,4),(4,0)]) ##Me conviene mas pensarlo como un diccionario donde tengo : Posicion, cantidades comidas, y comidas restantes.
+Obstaculos = ((0,3),(0,5),(1,3),(1,1),(2,2),(2,4),(3,0),(4,1),(4,3),(4,5),(5,3))
+def result(state, actions):
         origen, destino = actions
-        lo_que_falta_para_llenarse = (destino + 1) - state[destino] #Le resto a la posición mas uno el contenido para sacar el faltante que tiene ese jarro
-        #Proximo paso es trasvasar. Tengo que verificar que no se pase del contenido. Lo que puedi hacer es vaciar el jarro y el 
-        if (state[origen] > lo_que_falta_para_llenarse):
-            state[origen] -= lo_que_falta_para_llenarse
-            state[destino] += lo_que_falta_para_llenarse
-        else :
-            state[destino] += state[origen]
-            state[origen] = 0
-        return state
+        Comidas = state[2]
+        estado = list(state)
+        for pos,comida in enumerate(Comidas): ##Si hay una comida, la come
+            es_comida = (comida == destino)
+            if es_comida:
+                state[2].remove(comida)
+                estado[1] +=1 
+        estado [0] = destino
+        estado = tuple(estado)
+        return estado
+    
+resultados = result(Initial_state, ((3,5),(3,4)))
+print (resultados)
 
 
-Initial_state = [1,2,0,1]
-estado = result(Initial_state, (3,1))
 
-
-print(estado)
