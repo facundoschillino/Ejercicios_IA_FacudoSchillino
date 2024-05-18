bec04b124a55a1723f4a57adee1e80645fbce606
Facundo Gabriel Schillino Gariglio <facundoschillino01@gmail.com>
2024-05-18 13:57:33 -0300

    robots defensivos terminado

--- Ejercicio_Robots_Defensivos.py
+++ Ejercicio_Robots_Defensivos.py
@@ -12,12 +12,67 @@ from simpleai.search.viewers import BaseViewer, WebViewer
 
 Dimension_tablero = (3,4)
 Obstaculos = ((0,2),(2,1),(1,3))
-initial_state = ((0,1),(0,1))
-GOAL = ((3,2)(0,4))
+Initial_state = ((0,1),(0,1))
+GOAL = ((3,2),(0,4))
 
 class Robots_Defensivos(SearchProblem):
 
     def actions(self, state):
-        actions = [[],[]]
-        for cual_es, robot in enumerate(state):
+        actions = []
+        robot1,robot2= state
+        robot1Fila, Robot1Columna = robot1
+        robot2Fila, Robot2Columna = robot2
+        posibilidades = [] ##hago una lista de las posibles opciones que tienen los robots para moverse
+        posibilidades.append((1,robot1Fila-1, Robot1Columna))
+        posibilidades.append((1,robot1Fila+1,Robot1Columna))
+        posibilidades.append((1,robot1Fila, Robot1Columna+1))
+        posibilidades.append((1,robot1Fila, Robot1Columna -1))
+        posibilidades.append((2,robot2Fila-1, Robot2Columna))
+        posibilidades.append((2,robot2Fila+1,Robot2Columna))
+        posibilidades.append((2,robot2Fila, Robot2Columna+1))
+        posibilidades.append((2,robot2Fila, Robot2Columna -1))
+        for posible_casillero in posibilidades:
+            if posible_casillero[0] >= 0 and posible_casillero [0] <= 3 and posible_casillero[1] >= 0 and posible_casillero [0] <= 4: #Si esta dentro del rango y no hay un obstaculo es una opción
+                if (posible_casillero in Obstaculos) == False:
+                    actions.append(posible_casillero)
+        return actions
+    
+    def result(self, state, action):   
+        que_robot_es = action[0]
+        destino = (action[1], action[2])
+        state = list(state)
+        state[que_robot_es -1] = destino
+        state = tuple(state)
+        return state
+    
+    def is_goal(self, state):
+        return state == GOAL
+    
+    def cost(self, state, action, state2):
+        return 1
+    
+    def heuristics(self, state):
+        cuanto_falta = 0
+        for cual,robot in enumerate(state):
+            objetivo = GOAL[cual]
+            cuanto_falta = abs(robot[0] - objetivo[0])
+            cuanto_falta = abs(robot[1] - objetivo[1])
+        return cuanto_falta
+
+my_problem = Robots_Defensivos(Initial_state)
+
+#v = BaseViewer()
+v = WebViewer()
+result = astar(my_problem, 1000, viewer= v)
+
+if result is None:
+    print("No solution")
+else:
+    for action, state in result.path():
+        print("A:", action)
+        print("S:")
+        print(*state, sep="\n")
+
+    print("Final cost:", result.cost)    
             
+ 
--- Pruebas.py
+++ Pruebas.py
@@ -1,21 +1,42 @@
-GOAL = ((3,5),3, []) ##Posicion a la que tiene que llegar, cantidad de comidas que tiene que adquirir, posicion de las comidas
-Initial_state = ((3,5), 0, [(1,2),(3,4),(4,0)]) ##Me conviene mas pensarlo como un diccionario donde tengo : Posicion, cantidades comidas, y comidas restantes.
-Obstaculos = ((0,3),(0,5),(1,3),(1,1),(2,2),(2,4),(3,0),(4,1),(4,3),(4,5),(5,3))
-  def results(self, state, action):
-        origen, destino = actions
-        Comidas = state[2]
+
+Dimension_tablero = (3,4)
+Obstaculos = ((0,2),(2,1),(1,3))
+Initial_state = ((0,1),(0,1))
+GOAL = ((3,2),(0,4))
+
+def actions(state):
+        actions = []
+        robot1,robot2= state
+        robot1Fila, Robot1Columna = robot1
+        robot2Fila, Robot2Columna = robot2
+        posibilidades = [] ##hago una lista de las posibles opciones que tienen los robots para moverse
+        posibilidades.append((1,robot1Fila-1, Robot1Columna))
+        posibilidades.append((1,robot1Fila+1,Robot1Columna))
+        posibilidades.append((1,robot1Fila, Robot1Columna+1))
+        posibilidades.append((1,robot1Fila, Robot1Columna -1))
+        posibilidades.append((2,robot2Fila-1, Robot2Columna))
+        posibilidades.append((2,robot2Fila+1,Robot2Columna))
+        posibilidades.append((2,robot2Fila, Robot2Columna+1))
+        posibilidades.append((2,robot2Fila, Robot2Columna -1))
+        for posible_casillero in posibilidades:
+            if posible_casillero[0] >= 0 and posible_casillero [0] <= 3 and posible_casillero[1] >= 0 and posible_casillero [0] <= 4:
+                if (posible_casillero in Obstaculos) == False:
+                    actions.append(posible_casillero)
+        return actions
+def result(state, action):   
+        que_robot_es = action[0]
+        destino = (action[1], action[2])
         state = list(state)
-        for pos,comida in enumerate(Comidas): ##Si hay una comida, la come
-            es_comida = (comida == destino)
-            if es_comida:
-                state[2].remove(comida)
-                state[1] +=1 
-        state [0] = destino
+        state[que_robot_es -1] = destino
         state = tuple(state)
         return state
-    
-resultados = result(Initial_state, ((3,4),(3,3)))
-print (resultados)
+def heuristics(state):
+        cuanto_falta = 0
+        for cual,robot in enumerate(state):
+            objetivo = GOAL[cual]
+            cuanto_falta += abs(robot[0] - objetivo[0])
+            cuanto_falta += abs(robot[1] - objetivo[1])
+        return cuanto_falta
+acciones = heuristics(Initial_state)
 
-
-
+print (acciones)
