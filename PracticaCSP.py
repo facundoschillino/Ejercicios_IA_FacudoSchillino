ff8c50335a322862a39c8f387c012173542483cc
Facundo Gabriel Schillino Gariglio <facundoschillino01@gmail.com>
2024-05-22 12:14:06 -0300

    Defini los dominions del problema  "Mejorar al Millennium Falcon"

---
+++ PracticaCSP.py
@@ -0,0 +1,15 @@
+
+variables = ("P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8")
+doms = {
+    P1:"SO" , "SAA", "LTP"
+    P2:"SO", "SAA", "LTP"
+    P3:"SO", "SAA"
+    P4:"SAA"
+    P5:"SO", "BM", "BCM"
+    P6:"BM", "BCM"
+    P7:"MVDLL", "BM", "SE"
+    P8:"MVDLL", "BM", "SE"
+    }
+
+
+PARTES ()
