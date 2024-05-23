39c9fc805460ea2e1af843b7e05b132be14df557
Facundo Gabriel Schillino Gariglio <facundoschillino01@gmail.com>
2024-05-23 14:01:15 -0300

    Robots defensivos CSP

--- PracticaCSP.py
+++ PracticaCSP.py
@@ -1,61 +1,51 @@
 import itertools
-
 from simpleai.search import (CspProblem, backtrack, min_conflicts,
                              MOST_CONSTRAINED_VARIABLE,
                              LEAST_CONSTRAINING_VALUE,
                              HIGHEST_DEGREE_VARIABLE)
-
-variables = list(range(1,9))
-
-
-variab = ("P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8")
-dominios = {}
-posibilidades = {
-    'motor': [7,8],
-    'ocultamiento': [1, 2, 3, 5],
-    'apuntamiento': [1, 2, 3, 4],
-    'torpedos': [1, 2],
-    'medica': [5, 6, 7, 8],
-    'mejorada': [5, 6],
-    'escudo': variables,
-    'comunicacion': variables,
-    'evasion': [7, 8]}
-doms = {}
-INTERCONECTADAS = {
-    1: [2, 3],
-    2: [1, 3],
-    3: [2, 1],
-    4: [],
-    5: [6],
-    6: [5],
-    7: [8],
-    8: [7],
-}
-
-def diferentes (variables, valores): #No pueden repetirse mejoras, por lo que deben ser diferentes todas
-    for var1,var2 in valores:
-        if (var1 == var2):
-            return False
-        else:
-            return True
-def ocultamiento_vs_motor (variables, valores): #Si hay ocultamiento, no puede haber motor
-    return not ('motor' in valores and 'ocultamiento' in valores)
-
-def torpedo_with_apuntamiento(variables, valores): #En este metodo voy a revisar que esten ambas, y que esten conectadas
-    if ('torpedo' in valores and 'apuntamiento' in valores):
-        var1, var2 = variables
-        if var2 not in INTERCONECTADAS[var1]:
+variables = list(range(1,7))
+dominio = {}
+pos_disponibles = [(0,0), (0,1), (0,2), (0,3), (0,4), (1,0), (1,1), (1,2), (1,3), (1,4), (2,0), (2,1), (2,3), (2,4), (3,0), (3,1), (3,2), (3,3), (3,4)]
+restringidas= ((0,2),(1,3),(2,1))
+restricciones = []
+puertas_exteriores = ((0,4),(3,2))
+def alldif(variables, valores):
+    for var1, var2 in itertools.combinations(valores, 2):
+        if var1 == var2:
             return False
     return True
 
-def escudo_connected_with_comunicaciones(variables, valores):
-    if 'escudo' in valores and 'comunicacion' in valores:
-        var1, var2 = variables
-        if var1 in INTERCONECTADAS[var2]:
+def no_adyacentes(variables, valores):
+    posiciones = []
+    val1, val2 = valores
+    posiciones.append((val1[0]+1, val1[1]))
+    posiciones.append((val1[0]-1, val1[1]))
+    posiciones.append((val1[0], val1[1]+1))
+    posiciones.append((val1[0], val1[1]-1))
+    return val2 not in posiciones
+    
+    
+def no_restringidas(variables, valores):
+    for val in valores:
+        if val in restringidas:
             return False
     return True
 
 
 
+def in_puertas(variables, valores):
+    contador = 0
+    for val in valores:
+        if val in puertas_exteriores:
+            contador += 1
+    return contador == 2
 
-PARTES ()
+restricciones.append(((variables),in_puertas)) #aplica a todas
+for val1, val2 in itertools.combinations(variables,2):
+	restricciones.append(((val1,val2),alldif))
+	restricciones.append(((val1,val2),no_adyacentes))
+restricciones.append((variables, no_restringidas))
+problema = CspProblem(variables, {var: pos_disponibles for var in variables}, restricciones)
+# Resolución del problema usando backtracking
+solucion = backtrack(problema)
+print(solucion)
